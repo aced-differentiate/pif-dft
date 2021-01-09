a5eb82fe4343c127234021a9d42090c943d64683
Lance Kavalsky <lkavalsk@andrew.cmu.edu>
2021-01-08 21:46:34 -0500

    Start writing gpaw tests

---
+++ tests/parsers/test_gpaw.py
@@ -0,0 +1,27 @@
+import unittest
+from dfttopif.parsers.gpaw import GpawParser
+from ..test_pif import unpack_example, delete_example
+from pypif.obj.common.value import Value
+import os
+import shutil
+
+
+class TestGpawParser(unittest.TestCase):
+
+    def get_parser(self,name):
+        '''Get a GpawParser for a certain test'''
+        unpack_example(os.path.join('examples', 'gpaw', name+'.tar.gz'))
+        return GpawParser.generate_from_directory(name)
+
+    def test_H2(self):
+        """Test that a static calculation is parseable"""
+
+        # Parse the results
+        parser = self.get_parser("H2.txt")
+
+        # Test the settings
+        self.assertEquals("GPAW", parser.get_name())
+        self.assertEquals("19.8.1", parser.get_version_number())
+
+if __name__ == '__main__':
+    unittest.main()
